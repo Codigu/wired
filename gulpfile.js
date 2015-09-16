9974b701a6ad05fc535f86305055daac66913449
larrygeams.nst <larrygeamsparangan.nst@gmail.com>
2015-09-16 11:40:34 -0700

    Updated gulpfile.js and docs.php

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,31 @@
-var gulp 		= require('gulp'),
-	sass 		= require('gulp-sass'),
-	watch 		= require('gulp-watch'),
-	browserSync = require('browser-sync').create();
+var gulp 		     = require('gulp'),
+	 sass 		     = require('gulp-sass'),
+	 watch 		     = require('gulp-watch'),
+	 browserSync   = require('browser-sync').create(),
+   highlight     = require('gulp-highlight');
 
 gulp.task('sass', function () {
  return gulp.src('sass/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sass({outputStyle: 'compressed'}))
-    .pipe(gulp.dest('css'))
-    .pipe(browserSync.stream());
+            .pipe(sass().on('error', sass.logError))
+            .pipe(sass({outputStyle: 'compressed'}))
+            .pipe(gulp.dest('css'))
+            .pipe(browserSync.stream());
+});
+
+gulp.task('highlight', function () {
+    return gulp.src('dev/*.php')
+                .pipe(highlight())
+                .pipe(gulp.dest('docs'));
 });
  
 gulp.task('watch', function () {
    browserSync.init({
-        proxy: "http://larry/wired-master"
+        proxy: "http://localhost/wired"
     });	
 
   gulp.watch('sass/*.scss', ['sass']);
-  gulp.watch("*.php").on('change', browserSync.reload);
+  gulp.watch('dev/*.php', ['highlight']);
+  gulp.watch("dev/*.php").on('change', browserSync.reload);
 });
 
 gulp.task('default', ['watch']);
