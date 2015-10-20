564d5ca0eb91174a664fd051d088896a0b6a03cb
larrygeams.nst <larrygeamsparangan.nst@gmail.com>
2015-10-20 14:21:04 -0800

    Updated docs

--- dist/js/wired.js
+++ dist/js/wired.js
@@ -18,138 +18,92 @@
 
     /*
     * Add Smooth Scrolling Transition */
-    function smoothTarget(){
+    $('a.target[href^="#"]').click(function(event) {
 
-        $('a.target[href^="#"]').click(function(event) {
+        var id      = $(this).attr("href"),
+            target  = $(id).offset().top;
 
-            var id      = $(this).attr("href"),
-                target  = $(id).offset().top;
+        $('html, body').animate({ scrollTop:target }, 500);
 
-            $('html, body').animate({ scrollTop:target }, 500);
+        event.preventDefault();
+    });
 
-            event.preventDefault();
-        });
-
-    }
-
-    smoothTarget();
  
     /*
     * Offcanvas Menu 
     * Replaced Bootstrap's native collapsable menu */
+    $('[data-toggle="offcanvas"]').on('click', function(event){
+        var self    =   $(this),
+            target  =   self.data('target'),
+            body    =   document.body;
 
-    function offcanvasMenu(){
-        $('[data-toggle="offcanvas"]').on('click', function(){
-            var self    =   $(this),
-                target  =   self.data('target');
+        $(body).toggleClass('menu-open');
+        $(target).toggleClass('offcanvas-open');
 
-            $(target).toggleClass('offcanvas-open');
-        }); 
-    }
-
-    offcanvasMenu();
+        return false;
+    }); 
 
 
     /* InputFile
     * Adding the filename label to the input file button */
+    var inputs = $('.inputfile input[type="file"]');
 
-    function inputFile(){ 
-        var inputs = $('.inputfile input[type="file"]');
-
-        inputs.each(function(){ 
-            $(this).on('change', function(){
-                var self        =   $(this),
-                    fileName    =   '';
-                    fileName    =   self.val().split( '\\' ).pop();
-                
-                if(fileName){
-                    $(this).next('label').text(fileName);
-                }
-            });
+    inputs.each(function(){ 
+        $(this).on('change', function(){
+            var self        =   $(this),
+                fileName    =   '';
+                fileName    =   self.val().split( '\\' ).pop();
+            
+            if(fileName){
+                $(this).next('label').text(fileName);
+            }
         });
-    }
-
-    inputFile();
+    });
+   
 
     /* Tooltip.js
     * Hover over the links below to see tooltips */
-    function toolTip(){
-        $('[data-toggle="tooltip"]').tooltip();
-    }
-
-    toolTip();
-
+    $('[data-toggle="tooltip"]').tooltip();
 
     /* Popover.js
     * Add small overlays of content */
-    function popOver(){
-        $('[data-toggle="popover"]').popover()
-    }
-    
-    popOver();
-
+    $('[data-toggle="popover"]').popover()
 
     /* bootstrap-datepicker.js
     * A datepicker for Twitter Bootstrap */
-
-    function datePicker(){
-        $('[data-plugin="datepicker"]').datepicker();
-    }
-
-    datePicker();
+    $('[data-plugin="datepicker"]').datepicker();
 
     /* chosen.js
     * jQuery plugin that makes long, unwieldy select boxes much more user-friendly. */
-
-    function selectBox(){
-        $('[data-plugin="selectbox"]').chosen();
-    }
-
-    selectBox();
+    $('[data-plugin="selectbox"]').chosen();
 
     /* dropzone.js
     * An open source library that provides drag’n’drop file uploads with image previews. */
- 
-    function dropZone(){
-        $('[data-plugin="dropzone"]').dropzone();
-    }
- 
-    dropZone();
+    $('[data-plugin="dropzone"]').dropzone();
+
+    /* bootstrap-wysihtml5.js
+    * Simple, beautiful wysiwyg editors */
+    $('[data-plugin="wysihtml5"]').wysihtml5();
 
 
     /* sweetalert2.js
     * A beautiful and customizable replacement for Javascript's "Alert" */
- 
-    function sweetAlert(){
 
-        // Show basic message
-         $('[data-plugin="sweetAlert"]').on('click', function(){
-             swal('The Internet?', 'That thing is still around?');
-          });
+    // Show basic message
+     $('[data-plugin="sweetAlert"]').on('click', function(){
+         swal('The Internet?', 'That thing is still around?');
+      });
 
-        // Show success message
-        $('[data-plugin="sweetAlert"][data-alert="success"]').on('click', function(){
-           swal(   'Good job!',  'You clicked the button!',   'success' );
-        });
+    // Show success message
+    $('[data-plugin="sweetAlert"][data-alert="success"]').on('click', function(){
+       swal(   'Good job!',  'You clicked the button!',   'success' );
+    });
 
-        // Show error message
-        $('[data-plugin="sweetAlert"][data-alert="danger"]').on('click', function(){
-         swal(   'Oops...',  'Something went wrong!!',   'error' );
-        });
-    }
- 
-    sweetAlert();
-
-    /* bootstrap-wysihtml5.js
-    * Simple, beautiful wysiwyg editors */
-
-
-    function wysiHtml5(){ 
-        $('[data-plugin="wysihtml5"]').wysihtml5();
-    }
-
-    wysiHtml5();
-
+    // Show error message
+    $('[data-plugin="sweetAlert"][data-alert="danger"]').on('click', function(){
+     swal(   'Oops...',  'Something went wrong!!',   'error' );
+    });
+    
     
 
 })( jQuery ); 
