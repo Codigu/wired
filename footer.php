795f10c85bb457b3327c91ad973bc74d41025c29
Larry Geams Parangan <larrygeams@users.noreply.github.com>
2016-10-11 15:23:18 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,7 +0,0 @@
-	  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="plugins/jquery/jquery.min.js"><\/script>')</script>
-    <script src="plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="dist/js/wired.js"></script>
-
-  </body>
-</html>
