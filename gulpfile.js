cf113a937d2bcfe6ac054a2b7694d0aa1a3b236e
Larry <larry@macbook.local>
2016-10-14 14:06:54 +0800

    Updated grid framework and primary color

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,71 @@
-var gulp          = require('gulp');
-var browserify    = require('browserify');
-var sass          = require('gulp-sass');
-var watch         = require('gulp-watch');
-var rename        = require('gulp-rename');
-var uglify        = require('gulp-uglify');
-var pump          = require('pump');
-var source        = require('vinyl-source-stream');
-var runSequence   = require('run-sequence');
-var autoprefixer  = require('gulp-autoprefixer');
+var gulp = require('gulp');
+var browserify = require('browserify');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var source = require('vinyl-source-stream');
+var runSequence = require('run-sequence');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Convert Sass files into Css files
-gulp.task('scss', function () {
- return gulp.src('scss/**/*.scss')
-          .pipe(sass().on('error', sass.logError))
-          .pipe(autoprefixer())
-          .pipe(gulp.dest('dist/css'));
+gulp.task('scss', function() {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('minify-scss', function () {
- return gulp.src('scss/**/*.scss')
-          .pipe(sass().on('error', sass.logError))
-          .pipe(autoprefixer())
-          .pipe(sass({outputStyle: 'compressed'})) 
-          .pipe(rename({suffix: '.min'}))
-          .pipe(gulp.dest('dist/css'));
+gulp.task('minify-scss', function() {
+    return gulp.src('scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('dist/css'));
 });
 
 // Bundle all js files into one single file
-gulp.task('js', function () {
-  return browserify({ entries: ["js/wired.js"] })
+gulp.task('js', function() {
+    return browserify({ entries: ["js/wired.js"] })
         .bundle()
         .pipe(source('wired.js'))
         .pipe(gulp.dest('dist/js'));
 });
 
 // Compress js files
-gulp.task('minify-js', function (cb) { 
-   return gulp.src('js/wired.js')
-      .pipe(rename({suffix: '.min'}))
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/js'));
+gulp.task('minify-js', function(cb) {
+    return gulp.src('js/wired.js')
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Copy files from one directory to another
+gulp.task('copy', function() {
+
+    // Font Awesome 
+    gulp.src([
+        'node_modules/font-awesome/*/**'
+    ]).pipe(gulp.dest('dist/fonts'));
+
+    // jQuery
+    gulp.src([
+        'node_modules/jquery/dist/jquery.min.js'
+    ]).pipe(gulp.dest('plugins'));
 });
 
 // Build all assets from Sass to Css and compressed bundle js
-gulp.task('build', function(){
+gulp.task('build', function() {
     runSequence('scss', 'minify-scss', 'js', 'minify-js');
-}); 
- 
+});
+
 // Watch for every changes on both sass and js
-gulp.task('watch', function () { 
-  gulp.watch('**/*.scss', ['scss']); 
-  gulp.watch('**/*.js', ['js']); 
+gulp.task('watch', function() {
+    gulp.watch('**/*.scss', ['scss']);
+    gulp.watch('**/*.js', ['js']);
 });
 
 // Default Task 
-gulp.task('default', ['build']); 
+gulp.task('default', ['build']);
