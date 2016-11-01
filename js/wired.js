fc5f52e9002837edeabfeafacf1b0d304ddae0f8
Larry <larry@macbook.local>
2016-11-01 15:55:39 +0800

    Updated js

--- js/wired.js
+++ js/wired.js
@@ -1,5 +1,20 @@
 (function( $ ) {
+	// Wired
   require('./wired/wired.menu.js');
   require('./wired/wired.smooth.scroll.js');
+
+  // Bootstrap
+  require('../plugins/bootstrap/js/affix.js');
+  require('../plugins/bootstrap/js/alert.js');
+  require('../plugins/bootstrap/js/button.js');
+  require('../plugins/bootstrap/js/carousel.js');
+  require('../plugins/bootstrap/js/collapse.js');
+  require('../plugins/bootstrap/js/dropdown.js');
+  require('../plugins/bootstrap/js/modal.js');
+  require('../plugins/bootstrap/js/tooltip.js');
+  require('../plugins/bootstrap/js/popover.js');
+  require('../plugins/bootstrap/js/scrollspy.js');
+  require('../plugins/bootstrap/js/tab.js');
+  require('../plugins/bootstrap/js/transition.js');
 })( jQuery ); 
  
