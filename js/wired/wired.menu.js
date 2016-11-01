9e691397e9acc49261c2caa0e6a07354478c958b
Larry <larry@macbook.local>
2016-11-01 15:54:26 +0800

    Fixed issue

--- js/wired/wired.menu.js
+++ js/wired/wired.menu.js
@@ -1,35 +1,42 @@
 (function($) {
-    'use strict';
+  'use strict';
 
-    /*
-     * Toggle Menu
-     * offcanvas menu navigation */
-    const $body = $(document.body);
-    const $menu = $('.navbar-toggle');
-    const type = $menu.data('toggle');
-    const target = $menu.data('target');
+  /*
+   * Toggle Menu
+   * offcanvas menu navigation */
+  var type;
+  var target;
+  const $body = $(document.body);
+  const $menu = $('.navbar-toggle');
 
-    $menu.on('click', function(event) {
+  $menu.on('click', function(event) {
 
-        $body.toggleClass(type + '-open menu-open');
-        $(target).toggleClass('in');
+    let data_toggle = $(this).data('toggle');
+    let data_target = $(this).data('target');
 
+    type = data_toggle;  
+    target = data_target;  
+
+    $body.toggleClass(data_toggle + '-open menu-open');
+    $(data_target).toggleClass('in');
+
+    return false;
+  });  
+
+  // CLicking outside the target contianer 
+  $body.on('click', function(event) {
+
+    if ($body.hasClass(type + '-open')) {
+      if ($(event.target).is(target)) {
         return false;
-    });
+      } else if ($(event.target).hasClass('dropdown-toggle')) {
+        return true;
+      }
 
-    // CLicking outside the target contianer 
-    $body.on('click', function(event) {
-        if ($body.hasClass(type + '-open')) {
-            if ($(event.target).is(target)) {
-                return false; 
-            } else if ($(event.target).hasClass('dropdown-toggle')) {
-                return true;
-            }
-
-           $body.removeClass(type + '-open menu-open');
-           $(target).removeClass('in');
-        }
-    });
+      $body.removeClass(type + '-open menu-open');
+      $(target).removeClass('in');
+    }
+  });
 
 
 
